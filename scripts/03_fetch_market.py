64a61658378f6ab8b27482f51cf83db2e916ffd5
bzoinkr <156102628+bzoinkr@users.noreply.github.com>
2026-02-19 15:54:43 -0500

    Update 03_fetch_market.py

--- scripts/03_fetch_market.py
+++ scripts/03_fetch_market.py
@@ -3,8 +3,8 @@ from _bootstrap import *
 from datetime import date, timedelta
 
 from common.config import load_config
-from pipelines.peerCompany_pipeline import build_peerCompanies, save_keywords
-from pipelines.yahoo_news_pipeline import fetch_and_save_yahoo_news
+from apis.market_data import get_ticker_daily
+from apis.vix_data import get_vix_data
 
 
 def main():
@@ -13,18 +13,22 @@ def main():
 
     # Base tickers defined in the config universe
     tickers = run["universe"]["tickers"]
+    start_date = run["universe"]["start_date"]
+    end_date = run["universe"]["end_date"]
+    interval = run["frequency"]["base"] # might need to be 1d instead of 1D
 
-    stock_df = get_ticker_daily(ticker='NVDA',
-                  start_date='2026-01-02',
-                  end_date='2030-01-01',
+    stock_df = get_ticker_daily(ticker=tickers,
+                  start_date=start_date,
+                  end_date=end_date,
                   resample_freq='daily',
                   columns=None,
                   token='3d657ef651a029d6e8e71f6670282dfdb8877f8d')
   
-    vix_df = get_vix_data(period="1mo", interval="1d")
+    vix_df = get_vix_data(period="1mo", interval=interval)
 
     print(stock_df.head)
   
     print(vix_df.head)
 if __name__ == "__main__":
     main()
+

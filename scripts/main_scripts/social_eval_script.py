142c8192cdf10ff950a665a9c3598f88ea8c77f9
Joseob <133923804+Joseob@users.noreply.github.com>
2026-04-23 09:47:16 -0400

    minor changes

--- scripts/main_scripts/social_eval_script.py
+++ scripts/main_scripts/social_eval_script.py
@@ -1,8 +1,8 @@
 import os
 
-os.system("python scripts/032_fetch_market_volatility_compound.py")
-os.system("python scripts/042_fetch_social_train.py")
-os.system("python scripts/112_execute_vader_train.py")
-os.system("python scripts/212_clean_social_data_train.py")
+os.system("python scripts/train_social/032_fetch_market_volatility_compound.py")
+os.system("python scripts/train_social/042_fetch_social_train.py")
+os.system("python scripts/train_social/112_execute_vader_train.py")
+os.system("python scripts/train_social/212_clean_social_data_train.py")
 os.system("python scripts/40_createModel_social.py")
 os.system("python scripts/41_evaluate_social.py")
